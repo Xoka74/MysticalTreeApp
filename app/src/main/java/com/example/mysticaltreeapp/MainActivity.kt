bd2731b9090f6340eb38226fc6dbb8047deafd61
Xoka74 <ya.vladimir565@mail.ru>
2022-11-17 20:14:36 +0500

    Initial commit

---
+++ app/src/main/java/com/example/mysticaltreeapp/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.mysticaltreeapp
+
+import android.os.Bundle
+//import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //val imageList = listOf<Int>(
+        //    R.drawable.dog,
+        //    R.drawable.bunny_girl
+        //)
+        //val funnyCat = R.drawable.funny_cat
+        //val imageView = ImageView(this)
+        //val textView = TextView(this)
+        //imageView.setImageResource(funnyCat)
+    }
+}
